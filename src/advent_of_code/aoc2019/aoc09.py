9a15805c7842c4c00bfdef1dcf4b1e766d180e8b
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-10-02 19:38:55 -0500

    Add 2019, day 9 solution

---
+++ src/advent_of_code/aoc2019/aoc09.py
@@ -0,0 +1,83 @@
+"""Advent of Code 2019, Day 9: https://adventofcode.com/2019/day/9"""
+from math import floor, log10
+
+import pytest
+
+from advent_of_code.aoc2019.intcode import IntcodeMachine
+from advent_of_code.base import Solution
+
+
+class AocSolution(Solution[int]):
+    def __init__(self):
+        super().__init__(9, 2019)
+
+    def solve_part_one(self) -> int:
+        with self.open_input() as f:
+            program = IntcodeMachine.read_buffer(f)
+        machine = IntcodeMachine(program, iter((1,)))
+        result = list(machine.run())
+        assert len(result) == 1
+        return result[0]
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as f:
+            program = IntcodeMachine.read_buffer(f)
+        machine = IntcodeMachine(program, iter((2,)))
+        result = list(machine.run())
+        assert len(result) == 1
+        return result[0]
+
+
+def test_quine():
+    quine = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+    machine = IntcodeMachine(quine.copy())
+    result = list(machine.run())
+    assert result == quine
+
+
+def test_large_number():
+    program = [1102,34915192,34915192,7,4,7,99,0]
+    machine = IntcodeMachine(program)
+    result = next(machine.run())
+    assert floor(log10(result)) + 1 == 16
+
+
+def test_large_number_2():
+    program = [104,1125899906842624,99]
+    machine = IntcodeMachine(program.copy())
+    result = next(machine.run())
+    assert result == program[1]
+
+
+def test_read_past_initial_buffer():
+    program = [4, 100, 99]
+    machine = IntcodeMachine(program)
+    [result] = list(machine.run())
+    assert result == 0
+
+
+def test_write_past_initial_buffer():
+    program = [3, 100, 4, 100, 99]
+    machine = IntcodeMachine(program, input_stream=iter((42,)))
+    [result] = list(machine.run())
+    assert result == 42
+
+
+@pytest.mark.parametrize(
+    ("program", "expected"),
+    [
+        ([3, 100, 9, 1, 204, 99, 204, 0, 99], [0, 42]),
+        ([3, 100, 109, 1, 204, 99, 204, 0, 99], [42, 100]),
+    ]
+)
+def test_relative_mode(program, expected):
+    machine = IntcodeMachine(program, input_stream=iter((42,)))
+    result = list(machine.run())
+    assert result == expected
+
+
+def test_relative_mode_input():
+    program = [203, 2]
+    machine = IntcodeMachine(program, input_stream=iter((99,)))
+    result = list(machine.run())
+    assert result == []
--- src/advent_of_code/aoc2019/intcode.py
+++ src/advent_of_code/aoc2019/intcode.py
@@ -46,67 +46,71 @@ class IntcodeMachine:
     def process_opcode(self, op_code: int, modes: int) -> Optional[int]:
         output: Optional[int] = None
         match op_code:
-            case 1:  # add
+            case 1:  # add (day 2)
                 left, right = self.read_values(modes, 2)
-                result_idx = self.read(self.pointer + 3)
+                _, _, result_idx = self.read_address(modes, 3)
                 self.write(result_idx, left + right)
                 self.pointer += 4
-            case 2:  # multiply
+            case 2:  # multiply (day 2)
                 left, right = self.read_values(modes, 2)
-                result_idx = self.read(self.pointer + 3)
+                _, _, result_idx = self.read_address(modes, 3)
                 self.write(result_idx, left * right)
                 self.pointer += 4
-            case 3:  # input
-                address = self.read(self.pointer + 1)
+            case 3:  # input (day 5)
+                [address] = self.read_address(modes, 1)
                 self.write(address, next(self.input_stream))
                 self.pointer += 2
-            case 4:  # output
+            case 4:  # output (day 5)
                 [output] = self.read_values(modes, 1)
                 self.pointer += 2
-            case 5:  # jump-if-true
+            case 5:  # jump-if-true (day 5)
                 condition, address = self.read_values(modes, 2)
                 if condition:
                     self.pointer = address
                 else:
                     self.pointer += 3
-            case 6:  # jump-if-false
+            case 6:  # jump-if-false (day 5)
                 condition, address = self.read_values(modes, 2)
                 if not condition:
                     self.pointer = address
                 else:
                     self.pointer += 3
-            case 7:  # less than
+            case 7:  # less than (day 5)
                 left, right = self.read_values(modes, 2)
-                address = self.read(self.pointer + 3)
+                _, _, address = self.read_address(modes, 3)
                 self.write(address, int(left < right))
                 self.pointer += 4
-            case 8:  # equals
+            case 8:  # equals (day 5)
                 left, right = self.read_values(modes, 2)
-                address = self.read(self.pointer + 3)
+                _, _, address = self.read_address(modes, 3)
                 self.write(address, int(left == right))
                 self.pointer += 4
-            case 9:  # relative base adjustment
-                [adjustment] = self.read_values(modes, 1)
+            case 9:  # relative base adjustment (day 9)
+                adjustment = next(self.read_values(modes, 1))
                 self.relative_base += adjustment
                 self.pointer += 2
-            case 99:  # halt
+            case 99:  # halt (day 2)
                 self.pointer = None
             case _:
                 raise ValueError(f"Unrecognized op code: {op_code} at index {self.pointer}")
         return output
 
-    def read_values(self, modes: int, length: int) -> Iterator[int]:
+    def read_address(self, modes: int, length: int) -> Iterator:
         for offset in range(1, length + 1):
             mode = modes % 10
             modes //= 10
             match mode:
-                case 0:  # position mode
+                case 0:  # position mode (day 5)
                     index = self.read(self.pointer + offset)
-                    yield self.read(index)
-                case 1:  # immediate mode
-                    yield self.read(self.pointer + offset)
-                case 2:  # relative mode
-                    relative = self.read(self.pointer + offset)
-                    yield self.read(self.offset_base + relative)
+                    yield index
+                case 1:  # immediate mode (day 5)
+                    yield self.pointer + offset
+                case 2:  # relative mode (day 9)
+                    relative_offset = self.read(self.pointer + offset)
+                    yield self.relative_base + relative_offset
                 case _:
                     raise ValueError(f"Unrecognized parameter mode: {mode}")
+
+    def read_values(self, modes: int, length: int) -> Iterator[int]:
+        for address in self.read_address(modes, length):
+            yield self.read(address)
