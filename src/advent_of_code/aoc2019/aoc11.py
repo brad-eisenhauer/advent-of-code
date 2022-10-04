626350dd9fdbdff2ad9a0142567fc263b68084fa
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-10-03 20:09:05 -0500

    Add 2019, day 11, part 2 solution

--- src/advent_of_code/aoc2019/aoc11.py
+++ src/advent_of_code/aoc2019/aoc11.py
@@ -15,8 +15,18 @@ class AocSolution(Solution):
         robot.run()
         return len(robot.panels)
 
+    def solve_part_two(self) -> int:
+        with self.open_input() as f:
+            program = IntcodeMachine.read_buffer(f)
+        robot = Robot(program)
+        robot.panels[(0, 0)] = 1
+        robot.run()
+        robot.print_panels()
+
 
 class Robot:
+    BLOCK = "▒"
+
     def __init__(self, program):
         self.brain = IntcodeMachine(program, input_stream=self.eyes())
         self.panels: dict[tuple[int, int], int] = {}
@@ -43,11 +53,23 @@ class Robot:
         x, y = self.orientation
         match direction:
             case 0:  # turn left
-                self.orientation = -y, x
-            case 1:  # turn right
                 self.orientation = y, -x
+            case 1:  # turn right
+                self.orientation = -y, x
             case _:
                 raise ValueError(f"Unrecognized turn instruction: {direction}")
 
     def advance(self):
         self.location = tuple(a + b for a, b in zip(self.location, self.orientation))
+
+    def print_panels(self):
+        mins = maxes = (0, 0)
+        for panel in self.panels:
+            mins = tuple(min(*xs) for xs in zip(mins, panel))
+            maxes = tuple(max(*xs) for xs in zip(maxes, panel))
+        for row_idx in range(mins[1], maxes[1] + 1):
+            row = "".join(
+                self.BLOCK if self.panels.get((col_idx, row_idx)) else " "
+                for col_idx in range(mins[0], maxes[0] + 1)
+            )
+            print(row)
