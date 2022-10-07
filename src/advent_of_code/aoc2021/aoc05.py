76737f32d9acb9b7f62548378927c5d3c9f7af41
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-10-07 18:48:30 -0500

    Add 2021, day 5 solution

---
+++ src/advent_of_code/aoc2021/aoc05.py
@@ -0,0 +1,115 @@
+"""Advent of Code 2021, Day 05: https://adventofcode.com/2021/day/5"""
+from __future__ import annotations
+
+from collections import Counter
+from dataclasses import dataclass
+from io import StringIO
+from typing import Iterable, Iterator, Tuple
+
+import pytest
+
+from advent_of_code.base import Solution
+from advent_of_code.util import greatest_common_divisor
+
+
+class AocSolution(Solution[int]):
+    def __init__(self):
+        super().__init__(5, 2021)
+
+    def solve_part_one(self) -> int:
+        with self.open_input() as f:
+            segments = translate_to_segments(f.readlines())
+            intersection_counter = count_intersections(segments, orthogonal_only=True)
+        return sum(1 for p in intersection_counter if intersection_counter[p] > 1)
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as f:
+            segments = translate_to_segments(f.readlines())
+            intersection_counter = count_intersections(segments, orthogonal_only=False)
+        return sum(1 for p in intersection_counter if intersection_counter[p] > 1)
+
+
+@dataclass(frozen=True)
+class Vector:
+    coords: Tuple[int, ...]
+
+    def __add__(self, other: Vector) -> Vector:
+        return Vector.from_iterable(a + b for a, b in zip(self.coords, other.coords))
+
+    def __mul__(self, other: int) -> Vector:
+        return Vector.from_iterable(x * other for x in self.coords)
+
+    def __floordiv__(self, other: int) -> Vector:
+        return Vector.from_iterable(x // other for x in self.coords)
+
+    def __sub__(self, other: Vector) -> Vector:
+        return Vector.from_iterable(a - b for a, b in zip(self.coords, other.coords))
+
+    @property
+    def is_orthogonal(self) -> bool:
+        return sum(1 for c in self.coords if c != 0) < 2
+
+    @classmethod
+    def from_string(cls, coords: str) -> Vector:
+        return cls.from_iterable(int(x) for x in coords.split(","))
+
+    @classmethod
+    def from_iterable(cls, coords: Iterable[int]) -> Vector:
+        return cls(tuple(coords))
+
+
+def translate_to_segments(lines: Iterable[str]) -> Iterator[Tuple[Vector, Vector]]:
+    """Map input lines to line segments"""
+    for line in lines:
+        start, _, end = line.split()
+        yield Vector.from_string(start), Vector.from_string(end)
+
+
+def count_intersections(
+    segments: Iterable[Tuple[Vector, Vector]], orthogonal_only: bool = False
+) -> Counter[Vector, int]:
+    """Count the number of line segments intersecting each point"""
+    return Counter(
+        p
+        for start, end in segments
+        for p in generate_points_on_line_segment(start, end, orthogonal_only)
+    )
+
+
+def generate_points_on_line_segment(
+    start: Vector, end: Vector, orthogonal_only: bool
+) -> Iterator[Vector]:
+    """Generate all the discreet points through which the given segment passes"""
+    delta = end - start
+    if orthogonal_only and not delta.is_orthogonal:
+        return
+    increment = delta // greatest_common_divisor(*delta.coords)
+
+    point = start
+    yield point
+    while point != end:
+        point += increment
+        yield point
+
+
+TEST_INPUT = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+
+@pytest.mark.parametrize(("orthogonal_only", "expected"), ((True, 5), (False, 12)))
+def test_count_overlaps(orthogonal_only, expected):
+    with StringIO(TEST_INPUT) as fp:
+        counter = count_intersections(translate_to_segments(fp), orthogonal_only)
+
+    result = sum(1 for p in counter if counter[p] > 1)
+
+    assert expected == result
