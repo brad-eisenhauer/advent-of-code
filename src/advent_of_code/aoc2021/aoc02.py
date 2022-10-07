1898a34541b4bce120e66ef02a49e4ba8e9c83c1
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-10-07 16:31:35 -0500

    Add 2021, day 2, part 2 solution

---
+++ src/advent_of_code/aoc2021/aoc02.py
@@ -0,0 +1,97 @@
+"""AoC 2021, Day 02: https://adventofcode.com/2021/day/2"""
+from functools import reduce
+from io import StringIO
+from pathlib import Path
+from typing import NewType
+
+import numpy as np
+
+from advent_of_code.base import Solution
+
+Movement = NewType(
+    "Movement", np.ndarray  # Movement vector consists of displacement, ∆ aim
+)
+State = NewType("State", np.ndarray)  # State vector consists of x, y, aim
+
+
+def create_movement(displacement, delta_aim) -> Movement:
+    return Movement(np.array([[displacement, delta_aim]]))
+
+
+def create_state(x=0, y=0, aim=0) -> State:
+    return State(np.array([[x, y, aim]]))
+
+
+UNIT_MOVEMENTS = {
+    "forward": create_movement(1, 0),
+    "up": create_movement(0, -1),
+    "down": create_movement(0, 1),
+}
+
+
+class AocSolution(Solution[int]):
+    def __init__(self):
+        super().__init__(2, 2021)
+
+    def solve_part_one(self) -> int:
+        ...  # TODO: implement 2021, day 2, part 1
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as f:
+            final_state = run_simulation(f)
+        x, depth, _ = final_state[0]
+        return x * depth
+
+
+def main(input_path: Path):
+    with open(input_path) as fp:
+        final_state = run_simulation(fp)
+
+    print(final_state)
+    x, depth, _ = final_state[0]
+    print(x * depth)
+
+
+def run_simulation(input_stream):
+    movements = (translate_instruction(instruction) for instruction in input_stream)
+    initial_state = create_state()
+    final_state = reduce(move, movements, initial_state)
+    return final_state
+
+
+def translate_instruction(instruction: str) -> Movement:
+    # https://media.giphy.com/media/tyttpGTMMCADZR2YPZe/giphy.gif
+    direction, magnitude = instruction.split()
+    return int(magnitude) * UNIT_MOVEMENTS[direction]
+
+
+def move(initial: State, movement: Movement) -> State:
+    """
+    initial = [x0, y0, a0]
+    movement = [∆x, ∆a]
+    final = [x0 + ∆x, y0 + ∆x * a0, a0 + ∆a]
+    final = initial + [∆x, ∆x * a0, ∆a]
+    [∆x, ∆x * a0, ∆a] = movement . [[1, a0, 0],
+                                    [0, 0, 1]]
+    """
+    _, _, aim = initial[0]
+    transform = np.array([[1, aim, 0], [0, 0, 1]])
+    return initial + movement.dot(transform)
+
+
+TEST_INPUT = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+
+
+def test_main():
+    from numpy.testing import assert_array_equal
+
+    with StringIO(TEST_INPUT) as fp:
+        final_state = run_simulation(fp)
+
+    assert_array_equal(create_state(15, 60, 10), final_state)
