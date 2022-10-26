16f08e6acd5db048f0d6e2055fcbb3a8389dfc6c
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-10-26 16:14:49 -0500

    Finished 2020, day 12

--- src/advent_of_code/aoc2020/aoc12.py
+++ src/advent_of_code/aoc2020/aoc12.py
@@ -20,11 +20,18 @@ class AocSolution(Solution[int]):
             ship.navigate(f)
         return ship.calc_manhattan_dist()
 
+    def solve_part_two(self) -> int:
+        ship = Ship()
+        with self.open_input() as f:
+            ship.navigate_by_waypoint(f)
+        return ship.calc_manhattan_dist()
+
 
 @dataclass
 class Ship:
     position: Vector = (0, 0)
     heading: Vector = (1, 0)
+    waypoint: Vector = (10, 1)
 
     def navigate(self, instructions: TextIO):
         for line in instructions:
@@ -49,6 +56,31 @@ class Ship:
                     self.heading = -hx, -hy
                 case "F", _:
                     self.position = x + value * hx, y + value * hy
+                case _:
+                    raise ValueError(f"Unrecognized command: {line}")
+
+    def navigate_by_waypoint(self, instructions: TextIO):
+        for line in instructions:
+            line = line.strip()
+            command, value = line[:1], int(line[1:])
+            wx, wy = self.waypoint
+            match command, value:
+                case "N", _:
+                    self.waypoint = wx, wy + value
+                case "S", _:
+                    self.waypoint = wx, wy - value
+                case "E", _:
+                    self.waypoint = wx + value, wy
+                case "W", _:
+                    self.waypoint = wx - value, wy
+                case ("L", 90) | ("R", 270):
+                    self.waypoint = -wy, wx
+                case ("R", 90) | ("L", 270):
+                    self.waypoint = wy, -wx
+                case "L" | "R", 180:
+                    self.waypoint = -wx, -wy
+                case "F", _:
+                    self.position = tuple(p + value * w for p, w in zip(self.position, self.waypoint))
                 case _:
                     raise ValueError(f"Unrecognized command: {line}")
 
@@ -77,3 +109,9 @@ def test_navigate(sample_input):
     ship = Ship()
     ship.navigate(sample_input)
     assert ship.calc_manhattan_dist() == 25
+
+
+def test_navigate_by_waypoint(sample_input):
+    ship = Ship()
+    ship.navigate_by_waypoint(sample_input)
+    assert ship.calc_manhattan_dist() == 286
