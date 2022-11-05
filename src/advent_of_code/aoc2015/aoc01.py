fcedf5a32a1cf6c3b040b3c81c4f996a4ecbf1ae
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-11-05 15:11:34 -0500

    Retrofit 2015 AocSolution classes with **kwargs in __init__

--- src/advent_of_code/aoc2015/aoc01.py
+++ src/advent_of_code/aoc2015/aoc01.py
@@ -1,6 +1,5 @@
 """Advent of Code 2015, day 1: https://adventofcode.com/2015/day/1"""
 from collections import Counter
-from io import StringIO
 from typing import Optional
 
 import pytest
@@ -9,8 +8,8 @@ from advent_of_code.base import Solution
 
 
 class AocSolution(Solution[int]):
-    def __init__(self):
-        super().__init__(1, 2015)
+    def __init__(self, **kwargs):
+        super().__init__(1, 2015, **kwargs)
 
     def solve_part_one(self) -> int:
         with self.open_input() as f:
