92d5985ef392130ebc9c2fa386049db0a86374bb
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2022-12-01 07:05:33 -0600

    Add 2022, day 1 solution

---
+++ src/advent_of_code/aoc2022/aoc01.py
@@ -0,0 +1,70 @@
+"""Advent of Code 2022, day 1: https://adventofcode.com/2022/day/1"""
+from __future__ import annotations
+
+from io import StringIO
+from typing import Iterator, TextIO
+
+import pytest
+
+from advent_of_code.base import Solution
+
+
+class AocSolution(Solution[int, int]):
+    def __init__(self, **kwargs):
+        super().__init__(1, 2022, **kwargs)
+
+    def solve_part_one(self) -> int:
+        with self.open_input() as f:
+            return calc_max_calories(f, 1)
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as f:
+            return calc_max_calories(f, 3)
+
+
+def calc_max_calories(f: TextIO, n: int) -> int:
+    lines = f.readlines()
+
+    def generate_totals(lines=lines) -> Iterator[int]:
+        elf_calories = 0
+        for line in lines:
+            if line == "\n":
+                yield elf_calories
+                elf_calories = 0
+            else:
+                elf_calories += int(line.strip())
+        yield elf_calories
+
+    elf_totals = sorted(generate_totals(), reverse=True)
+    return sum(elf_totals[:n])
+
+
+SAMPLE_INPUTS = [
+    """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+""",
+]
+
+
+@pytest.fixture
+def sample_input():
+    with StringIO(SAMPLE_INPUTS[0]) as f:
+        yield f
+
+
+@pytest.mark.parametrize(("n", "expected"), [(1, 24000), (3, 45000)])
+def test_calc_max_calories(sample_input, n, expected):
+    assert calc_max_calories(sample_input, n) == expected
