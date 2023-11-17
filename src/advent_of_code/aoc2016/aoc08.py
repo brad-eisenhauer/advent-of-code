e138f075e5486769c6d4692319dedec77f4e0d7c
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2023-11-17 16:46:32 -0600

    Add 2016, days 8-9 solutions

---
+++ src/advent_of_code/aoc2016/aoc08.py
@@ -0,0 +1,88 @@
+"""Advent of Code 2016, day 8: https://adventofcode.com/2016/day/8"""
+from __future__ import annotations
+
+import re
+import sys
+from dataclasses import dataclass, field
+from io import StringIO
+from itertools import product
+from typing import IO, ClassVar
+
+import pytest
+
+from advent_of_code.base import Solution
+from advent_of_code.cli import log
+from advent_of_code.util import ocr
+
+
+class AocSolution(Solution[int, int]):
+    def __init__(self, **kwargs):
+        super().__init__(8, 2016, **kwargs)
+
+    def solve_part_one(self) -> int:
+        grid = DisplayGrid()
+        with self.open_input() as fp:
+            grid.mutate(fp)
+        grid.print()
+        return sum(1 for row in grid.grid for char in row if char == DisplayGrid.BLOCK)
+
+    def solve_part_two(self) -> int:
+        grid = DisplayGrid()
+        with self.open_input() as fp:
+            grid.mutate(fp)
+        return ocr.to_string(["".join(row) for row in grid.grid], block_char=DisplayGrid.BLOCK)
+
+
+@dataclass
+class DisplayGrid:
+    grid: list[list[str]] = field(default_factory=lambda: [[" "] * 50 for _ in range(6)])
+    BLOCK: ClassVar[str] = "▒"
+
+    def rect(self, a: int, b: int):
+        for row, col in product(range(b), range(a)):
+            self.grid[row][col] = self.BLOCK
+
+    def rotate_row(self, y: int, by: int):
+        row_len = len(self.grid[y])
+        new_row = [self.grid[y][(x - by) % row_len] for x in range(row_len)]
+        self.grid[y] = new_row
+
+    def rotate_col(self, x: int, by: int):
+        col_len = len(self.grid)
+        new_col = [self.grid[(y - by) % col_len][x] for y in range(col_len)]
+        for y in range(col_len):
+            self.grid[y][x] = new_col[y]
+
+    def print(self, out: IO = sys.stdout):
+        for row in self.grid:
+            out.write("".join(row) + "\n")
+
+    def mutate(self, instructions: IO):
+        rect_pattern = r"rect (?P<a>\d+)x(?P<b>\d+)"
+        rot_row_pattern = r"rotate row y=(?P<y>\d+) by (?P<by>\d+)"
+        rot_col_pattern = r"rotate column x=(?P<x>\d+) by (?P<by>\d+)"
+
+        for line in instructions:
+            if (match := re.match(rect_pattern, line)) is not None:
+                a = int(match.groupdict()["a"])
+                b = int(match.groupdict()["b"])
+                self.rect(a, b)
+            elif (match := re.match(rot_row_pattern, line)) is not None:
+                y = int(match.groupdict()["y"])
+                by = int(match.groupdict()["by"])
+                self.rotate_row(y, by)
+            elif (match := re.match(rot_col_pattern, line)) is not None:
+                x = int(match.groupdict()["x"])
+                by = int(match.groupdict()["by"])
+                self.rotate_col(x, by)
+            else:
+                raise ValueError(f"'{line}' did not match any instruction.")
+
+
+def test_grid():
+    grid = DisplayGrid()
+    grid.rect(3, 2)
+    grid.rotate_col(1, 1)
+    grid.rotate_row(0, 4)
+    grid.rotate_col(1, 1)
+    grid.print()
---
+++ src/advent_of_code/aoc2016/aoc09.py
@@ -0,0 +1,56 @@
+"""Advent of Code 2016, day 9: https://adventofcode.com/2016/day/9"""
+from __future__ import annotations
+
+import re
+
+import pytest
+
+from advent_of_code.base import Solution
+from advent_of_code.cli import log
+
+
+class AocSolution(Solution[int, int]):
+    def __init__(self, **kwargs):
+        super().__init__(9, 2016, **kwargs)
+
+    def solve_part_one(self) -> int:
+        with self.open_input() as fp:
+            return decompressed_len(fp.read().strip())
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as fp:
+            return decompressed_len(fp.read().strip(), recursive=True)
+
+
+def decompressed_len(text: str, recursive: bool = False) -> int:
+    pattern = r"(?P<prefix>.*?)(\((?P<char_count>\d+)x(?P<rep_count>\d+)\))"
+    result = 0
+    while (match := re.match(pattern, text)) is not None:
+        result += len(match.groupdict()["prefix"])
+        text = text[match.end() :]
+        char_count = int(match.groupdict()["char_count"])
+        rep_count = int(match.groupdict()["rep_count"])
+        chunk = text[:char_count]
+        result += rep_count * (decompressed_len(chunk, recursive) if recursive else len(chunk))
+        text = text[char_count:]
+    result += len(text)
+    return result
+
+
+@pytest.mark.parametrize(
+    ("compressed_text", "recursive", "expected"),
+    [
+        ("ADVENT", False, 6),
+        ("A(1x5)BC", False, 7),
+        ("(3x3)XYZ", False, 9),
+        ("A(2x2)BCD(2x2)EFG", False, 11),
+        ("(6x1)(1x3)A", False, 6),
+        ("X(8x2)(3x3)ABCY", False, 18),
+        ("(3x3)XYZ", True, 9),
+        ("X(8x2)(3x3)ABCY", True, 20),
+        ("(27x12)(20x12)(13x14)(7x10)(1x12)A", True, 241920),
+        ("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", True, 445),
+    ],
+)
+def test_decompress(compressed_text, recursive, expected):
+    assert decompressed_len(compressed_text, recursive) == expected
