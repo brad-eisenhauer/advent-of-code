e908f28b13180118472c865c4d881e2d324c8409
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2023-11-19 08:01:35 -0600

    Add 2016, days 12-13 solutions

---
+++ src/advent_of_code/aoc2016/aoc12.py
@@ -0,0 +1,98 @@
+"""Advent of Code 2016, day 12: https://adventofcode.com/2016/day/12"""
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass, field
+from io import StringIO
+
+import pytest
+
+from advent_of_code.base import Solution
+
+
+class AocSolution(Solution[int, int]):
+    def __init__(self, **kwargs):
+        super().__init__(12, 2016, **kwargs)
+
+    def solve_part_one(self) -> int:
+        interpreter = AssembunnyInterpreter()
+        with self.open_input() as fp:
+            interpreter.run(fp.readlines())
+        return interpreter.registers["a"]
+
+    def solve_part_two(self) -> int:
+        interpreter = AssembunnyInterpreter()
+        interpreter.registers["c"] = 1
+        with self.open_input() as fp:
+            interpreter.run(fp.readlines())
+        return interpreter.registers["a"]
+
+
+@dataclass
+class AssembunnyInterpreter:
+    registers: dict[str, int] = field(default_factory=lambda: {"a": 0, "b": 0, "c": 0, "d": 0})
+    pointer: int = 0
+    program: list[str] = field(default_factory=list)
+
+    def run(self, program: list[str]):
+        self.program = program
+        self.pointer = 0
+        while self.pointer < len(self.program):
+            self.execute_instruction()
+
+    def execute_instruction(self):
+        pattern = r"(?P<inst>[a-z]{3}) (?P<arg1>[a-d]|-?[0-9]+)(?: (?P<arg2>[a-d]|-?[0-9]+))?"
+        match = re.match(pattern, self.program[self.pointer])
+        match match.groupdict()["inst"]:
+            case "cpy":
+                value = match.groupdict()["arg1"]
+                if value.isnumeric():
+                    value = int(value)
+                else:
+                    value = self.registers[value]
+                dest = match.groupdict()["arg2"]
+                self.registers[dest] = value
+                self.pointer += 1
+            case "inc":
+                register = match.groupdict()["arg1"]
+                self.registers[register] += 1
+                self.pointer += 1
+            case "dec":
+                register = match.groupdict()["arg1"]
+                self.registers[register] -= 1
+                self.pointer += 1
+            case "jnz":
+                value = match.groupdict()["arg1"]
+                if value.isnumeric():
+                    value = int(value)
+                else:
+                    value = self.registers[value]
+                jump = int(match.groupdict()["arg2"])
+                if value == 0:
+                    self.pointer += 1
+                else:
+                    self.pointer += jump
+
+
+SAMPLE_INPUTS = [
+    """\
+cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a
+""",
+]
+
+
+@pytest.fixture
+def sample_input():
+    with StringIO(SAMPLE_INPUTS[0]) as f:
+        yield f
+
+
+def test_run(sample_input):
+    interpreter = AssembunnyInterpreter()
+    interpreter.run(sample_input.readlines())
+    assert interpreter.registers["a"] == 42
---
+++ src/advent_of_code/aoc2016/aoc13.py
@@ -0,0 +1,84 @@
+"""Advent of Code 2016, day 13: https://adventofcode.com/2016/day/13"""
+from __future__ import annotations
+
+from collections import deque
+from typing import Iterator
+
+from advent_of_code.base import Solution
+from advent_of_code.util.pathfinder import AStar
+
+
+class AocSolution(Solution[int, int]):
+    def __init__(self, **kwargs):
+        super().__init__(13, 2016, **kwargs)
+
+    def solve_part_one(self) -> int:
+        with self.open_input() as fp:
+            favorite_value = int(fp.read().strip())
+        navigator = Navigator(favorite_value)
+        return navigator.find_min_cost_to_goal((1, 1))
+
+    def solve_part_two(self) -> int:
+        with self.open_input() as fp:
+            favorite_value = int(fp.read().strip())
+        bfs = BFS(favorite_value)
+        return sum(1 for _ in bfs.visit_to_depth((1, 1), 50))
+
+
+Vector = tuple[int, ...]
+directions: list[Vector] = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+class Navigator(AStar[Vector]):
+    def __init__(self, favorite_value: int, destination: Vector = (31, 39)):
+        self.favorite_value = favorite_value
+        self.destination = destination
+
+    def is_goal_state(self, state: Vector) -> bool:
+        return state == self.destination
+
+    def heuristic(self, state: Vector) -> int:
+        return sum(abs(a - b) for a, b in zip(state, self.destination))
+
+    def generate_next_states(self, state: Vector) -> Iterator[tuple[int, Vector]]:
+        for d in directions:
+            next_loc = tuple(a + b for a, b in zip(state, d))
+            if not self.is_wall(next_loc):
+                yield 1, next_loc
+
+    def is_wall(self, state: Vector) -> bool:
+        if any(c < 0 for c in state):
+            return True
+        x, y = state
+        discriminant = x * x + 3 * x + 2 * x * y + y + y * y + self.favorite_value
+        return density(discriminant) % 2 > 0
+
+
+def density(n: int) -> int:
+    result = 0
+    while n > 0:
+        result += n & 1
+        n >>= 1
+    return result
+
+
+class BFS:
+    def __init__(self, favorite_value: int):
+        self.navigator = Navigator(favorite_value)
+
+    def visit_to_depth(self, initial_loc: Vector, depth: int) -> Iterator[Vector]:
+        visited: set[Vector] = set([initial_loc])
+        frontier: deque[tuple[int, Vector]] = deque()
+        frontier.append((0, initial_loc))
+
+        while frontier:
+            current_depth, current_loc = frontier.popleft()
+            yield current_loc
+            next_depth = current_depth + 1
+            if next_depth > depth:
+                continue
+            for _, loc in self.navigator.generate_next_states(current_loc):
+                if loc in visited:
+                    continue
+                visited.add(loc)
+                frontier.append((next_depth, loc))
