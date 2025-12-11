af0e9e18f90587d93d878e74054b525b7c5c0e1e
Brad Eisenhauer <bradley.eisenhauer@gmail.com>
2025-12-11 16:46:58 -0600

    Implement DisjointSet for day 8 solution

--- src/advent_of_code/aoc2025/aoc08.py
+++ src/advent_of_code/aoc2025/aoc08.py
@@ -2,18 +2,24 @@
 
 from __future__ import annotations
 
-from functools import reduce
 import logging
-from io import StringIO
-from itertools import combinations
 import operator
-from typing import IO, Collection, Iterator, Optional, TypeAlias
+from collections import defaultdict
+from dataclasses import dataclass, field
+from functools import reduce
+from heapq import heapify, heappop
+from io import StringIO
+from itertools import combinations, islice
+from typing import IO, Iterable, Iterator, Optional, Self, TypeAlias
 
 import pytest
 
 from advent_of_code.base import Solution
 
 log = logging.getLogger(__name__)
+
+JunctionBox: TypeAlias = tuple[int, int, int]
+Connection: TypeAlias = tuple[JunctionBox, JunctionBox]
 
 
 class AocSolution(Solution[int, int]):
@@ -22,81 +28,78 @@ class AocSolution(Solution[int, int]):
 
     def solve_part_one(self, input_file: Optional[IO] = None, n_connections=1000) -> int:
         with input_file or self.open_input() as fp:
-            boxes = list(read_boxes(fp))
-        connections = make_shortest_connections(boxes, n_connections)
-        circuits = create_circuits(boxes, connections)
-        largest = sorted((len(c) for c in circuits), reverse=True)[:3]
-        return reduce(operator.mul, largest)
+            ds: DisjointSet[JunctionBox] = DisjointSet.from_elements(eval(line) for line in fp)
+        for left, right in islice(generate_shortest_connections(ds), n_connections):
+            ds.union(left, right)
+        circuit_sizes = ds.calc_set_sizes()
+        largest_sizes = sorted(circuit_sizes.values(), reverse=True)[:3]
+        return reduce(operator.mul, largest_sizes)
 
     def solve_part_two(self, input_file: Optional[IO] = None) -> int:
         with input_file or self.open_input() as fp:
-            boxes = list(read_boxes(fp))
-        *_, (last_left, last_right) = connect_until_single_circuit(boxes)
-        return last_left[0] * last_right[0]
+            ds: DisjointSet[JunctionBox] = DisjointSet.from_elements(eval(line) for line in fp)
+        all_roots = set(ds.parents)
+        for left, right in generate_shortest_connections(ds):
+            if (merged_root := ds.union(left, right)) is not None:
+                all_roots.remove(merged_root)
+            if len(all_roots) == 1:
+                return left[0] * right[0]
 
 
-JunctionBox: TypeAlias = tuple[int, int, int]
-Connection: TypeAlias = tuple[JunctionBox, JunctionBox]
+@dataclass
+class DisjointSet[T]:
+    parents: dict[T, T] = field(default_factory=dict)
+    ranks: dict[T, int] = field(default_factory=dict)
+
+    @classmethod
+    def from_elements(cls, items: Iterable[T]) -> Self:
+        parents = {item: item for item in items}
+        ranks = dict.fromkeys(parents, 0)
+        return cls(parents=parents, ranks=ranks)
+
+    def find(self, item: T) -> T:
+        if item not in self.parents:
+            self.parents[item] = item
+            self.ranks[item] = 0
+        if self.parents[item] != item:
+            self.parents[item] = self.find(self.parents[item])
+        return self.parents[item]
+
+    def union(self, left: T, right: T) -> T | None:
+        root_left = self.find(left)
+        root_right = self.find(right)
+
+        if root_left == root_right:
+            return None
+
+        if self.ranks[root_left] > self.ranks[root_right]:
+            self.parents[root_right] = root_left
+            return root_right
+        elif self.ranks[root_left] < self.ranks[root_right]:
+            self.parents[root_left] = root_right
+            return root_left
+        else:
+            self.parents[root_right] = root_left
+            self.ranks[root_left] += 1
+            return root_right
+
+    def calc_set_sizes(self) -> dict[T, int]:
+        result = defaultdict(int)
+        for item in self.parents:
+            result[self.find(item)] += 1
+        return result
+
+
+def generate_shortest_connections(boxes: DisjointSet[JunctionBox]) -> Iterator[Connection]:
+    connections = combinations(boxes.parents, 2)
+    connection_heap = [(calc_distance_sq(*pair), pair) for pair in connections]
+    heapify(connection_heap)
+    while connection_heap:
+        yield heappop(connection_heap)[1]
 
 
 def calc_distance_sq(left: JunctionBox, right: JunctionBox) -> int:
     return sum((a - b) ** 2 for a, b in zip(left, right))
-
-
-def read_boxes(reader: IO) -> Iterator[JunctionBox]:
-    for line in reader:
-        yield eval(line.strip())
-
-
-def make_shortest_connections(boxes: Collection[JunctionBox], n: int = 1000) -> list[Connection]:
-    box_pairs = combinations(boxes, 2)
-    shortest_pairs = sorted(box_pairs, key=lambda pair: calc_distance_sq(*pair))
-    return shortest_pairs[:n]
-
-
-def create_circuits(boxes: list[JunctionBox], connections: list[Connection]) -> list[set[JunctionBox]]:
-    circuit_map = {box: {box} for box in boxes}
-    for left, right in connections:
-        if circuit_map[left] is circuit_map[right]:
-            continue
-        circuit_map[left] |= circuit_map[right]
-        for box in circuit_map[left]:
-            circuit_map[box] = circuit_map[left]
-    # filter for unique values
-    result = []
-    for circuit in circuit_map.values():
-        if circuit not in result:
-            result.append(circuit)
-    return result
-
-
-def connect_until_single_circuit(boxes: Collection[JunctionBox]) -> list[Connection]:
-    box_pairs = combinations(boxes, 2)
-    shortest_pairs = sorted(box_pairs, key=lambda pair: calc_distance_sq(*pair))
-    circuit_map = {box: {box} for box in boxes}
-    # We'll need at least n-1 connection to join n boxes.
-    result = shortest_pairs[:len(boxes) - 1]
-    for left, right in shortest_pairs[:len(boxes) - 1]:
-        if circuit_map[left] is circuit_map[right]:
-            continue
-        circuit_map[left] |= circuit_map[right]
-        for box in circuit_map[left]:
-            circuit_map[box] = circuit_map[left]
-    if len(next(iter(circuit_map.values()))) == len(boxes):
-        return result
-    # Make additional connections until one circuit contains all of the boxes
-    for pair in shortest_pairs[len(boxes) - 1:]:
-        left, right = pair
-        if circuit_map[left] is circuit_map[right]:
-            continue
-        circuit_map[left] |= circuit_map[right]
-        result.append(pair)
-        if len(circuit_map[left]) == len(boxes):
-            return result
-        for box in circuit_map[left]:
-            circuit_map[box] = circuit_map[left]
-    raise ValueError("no solution found")
-
 
 
 SAMPLE_INPUTS = [
@@ -125,13 +128,13 @@ SAMPLE_INPUTS = [
 ]
 
 
-@pytest.fixture()
+@pytest.fixture
 def sample_input(request):
     with StringIO(SAMPLE_INPUTS[getattr(request, "param", 0)]) as f:
         yield f
 
 
-@pytest.fixture()
+@pytest.fixture
 def solution():
     return AocSolution()
 
